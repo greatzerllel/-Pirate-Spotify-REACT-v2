e9d352cea1b3d1bee9350e209e11720d3275deff
cgabrielP <cgabriel2303@gmail.com>
2022-09-26 21:21:39 +0000

    botones de prev y next

--- src/js/component/Botones.jsx
+++ src/js/component/Botones.jsx
@@ -4,18 +4,32 @@ import * as config from './config';
 const Botones = () => {
   let audioRef = useRef(null);
   const [music, setMusic] = useState([]);
-  const[isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [trackIndex, setTrackIndex] = useState(0);
+  let songNow = null
+  const songURL = "https://assets.breatheco.de/apis/sound/";
   
-  const setMusicNow = (url) => { 
-    const songURL = "https://assets.breatheco.de/apis/sound/";
+  const setMusicNow = (url) => {
+
     audioRef.current.src = songURL + url
   }
-
-
-  const [trackIndex, setTrackIndex] = useState(0);
+  //funcion que adelanta musica
+  const NextSong = (i, song) => {
+    i++;
+    songNow = song[i].url
+    audioRef.current.src = songURL + songNow
+    setTrackIndex(i)
+  }
+  const prevSong = (i, song) => {
+    i--;
+    songNow = song[i].url
+    audioRef.current.src = songURL + songNow
+    setTrackIndex(i)
+    trackIndex==undefined? setTrackIndex(0): i--;
+  }
 
   useEffect(() => {
-    
+
     getMusicAsync();
   }, [])
   const getMusicAsync = async () => {
@@ -49,29 +63,34 @@ const Botones = () => {
         music.length > 0 &&
         music.map((music, index) => {
           return (
-            <li className="list-group-item" type='button' key={index} onClick={() => { setMusicNow(music.url) }}>
-              {music.id} <b>{music.name.toUpperCase()}</b>
+            <li className="list-group-item" type='button' key={index} onClick={() => { setMusicNow(music.url), setTrackIndex(index), audioRef.current.play(), setIsPlaying(!isPlaying)}} >
+              <b>{music.id}</b> {music.name.toUpperCase()}
             </li>
           )
         })
       }
       </ul>
 
-      <audio ref={audioRef}  />
+      <audio ref={audioRef} />
 
       <div className="btn-group mt-3 d-flex justify-content-center" role="group" aria-label="Basic example">
-        <button type="button" className='form-control border border-0 py-3 px-5' onClick={() => { alert('soy el boton de retroceder') }} ><i className="fa-solid fa-backward " /></button>
+        <button type="button" className='form-control border border-0 py-3 px-5' onClick={() => { prevSong(trackIndex, music),audioRef.current.play() }} ><i className="fa-solid fa-backward " /></button>
 
         <button type="button" className=' form-control border border-0 py-3 mx-2 ' onClick={() => {
 
 
-          isPlaying ? ( audioRef.current.pause(), setIsPlaying(!isPlaying)) : (audioRef.current.play(), setIsPlaying(!isPlaying));
+          isPlaying ? (audioRef.current.pause(), setIsPlaying(!isPlaying)) : (audioRef.current.play(), setIsPlaying(!isPlaying));
 
           console.log(audioRef)
         }}>
-          <i className={"fa-solid " + (isPlaying? 'fa-pause' : 'fa-play')} /></button>
+          <i className={"fa-solid " + (isPlaying ? 'fa-pause' : 'fa-play')} /></button>
 
-        <button type="button" className='form-control border border-0 py-3 px-5' onClick={() => { alert('soy el boton de retroceder') }}><i className="fa-solid fa-forward " /></button>
+        <button type="button" className='form-control border border-0 py-3 px-5' onClick={
+          () => { NextSong(trackIndex, music),audioRef.current.play()}}>
+
+
+          <i className="fa-solid fa-forward" />
+        </button>
       </div>
 
     </>
